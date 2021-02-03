020b8586c08e078f393162f6f0ccc7e975ad4794
Karan Kangude <karankangude17@gmail.com>
2021-02-03 18:15:55 +0530

    navbar component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/layout/Navbar';
 function App() {
   return (
     <div className='App'>
+      <Navbar />
       <h1>React App</h1>
     </div>
   );
