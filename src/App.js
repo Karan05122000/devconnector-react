b3f5e8392ec1198422612ba7f6e0fd472b3ba189
Karan Kangude <karankangude17@gmail.com>
2021-02-03 18:27:33 +0530

    landing page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
 function App() {
   return (
     <div className='App'>
       <Navbar />
-      <h1>React App</h1>
+      <Landing />
       <Footer />
     </div>
   );
