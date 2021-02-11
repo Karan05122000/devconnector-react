80218e766c0f4d0c5a70770c2d7900d805eadb03
Karan Kangude <karankangude17@gmail.com>
2021-02-11 09:24:07 +0530

    auth reducers changed

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,13 +1,18 @@
+import isEmpty from '../validation/is-empty';
+
+import { SET_CURRENT_USER } from '../actions/types';
+
 const initialState = {
-  user: {},
   isAuthenticated: false,
+  user: {},
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    case 'TEST_DISPATCH':
+    case SET_CURRENT_USER:
       return {
         ...state,
+        isAuthenticated: !isEmpty(action.payload),
         user: action.payload,
       };
     default:
